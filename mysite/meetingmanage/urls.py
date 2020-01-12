29fdecc5f4aa1894095d13fb1e15948bf24d433d
cjl <11007446@qq.com>
2020-01-12 22:36:29 +0800

    加入zui 框架

--- mysite/meetingmanage/urls.py
+++ mysite/meetingmanage/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     # ex: /meetingmanage/
     path('', views.index, name='index'),
+    path('test/', views.test, name='test'),
     path('gendata/', views.index, name='gendata'),
+
 ]
